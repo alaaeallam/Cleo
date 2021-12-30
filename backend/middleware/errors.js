46f86872de25cb08cbc4d4b54f929f26a6af9ecd
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2021-12-30 23:59:30 +0200

    Validation & Mongoose IDError

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -2,7 +2,10 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
+
   if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    console.log(err);
+
     res.status(err.statusCode).json({
       success: false,
       error: err,
@@ -10,12 +13,26 @@ module.exports = (err, req, res, next) => {
       stack: err.stack,
     });
   }
+
   if (process.env.NODE_ENV === 'PRODUCTION') {
     let error = { ...err };
+
     error.message = err.message;
-    res.status(error.statusCode).json({
+
+    // Wrong Mongoose Object ID Error
+    if (err.name === 'CastError') {
+      const message = `Resource not found. Invalid: ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+    // Handling Mongoose Validation Error
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
+    res.status(error.statusCode || 500).json({
       success: false,
-      message: err.message || 'Internal Server Error',
+      message: error.message || 'Internal Server Error',
     });
   }
 };
