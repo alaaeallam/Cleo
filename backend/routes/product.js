86491bcf7c62795d3fe0afbee1992f5ae9a1e0ae
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2021-12-30 20:01:04 +0200

    API for delete product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,11 +6,13 @@ const {
   newProduct,
   getSingleProduct,
   updateProduct,
+  deleteProduct,
 } = require('../controllers/productControllers');
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/product/new').post(newProduct);
 router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id').delete(deleteProduct);
 
 module.exports = router;
