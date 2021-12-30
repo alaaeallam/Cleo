19528fc7fd330314e049eaafacde880e9fc118e9
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2021-12-30 22:25:15 +0200

    Creating Error Handler class and Middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-
+const errorMiddleWare = require('./middleware/errors');
 app.use(express.json());
 //Import all the routes
 
 const products = require('./routes/product');
 
 app.use('/api/v1', products);
+
+//Middleware to handle errors
+app.use(errorMiddleWare);
 module.exports = app;
--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const ErrorHandler = require('../utils/errorHandler');
 // Create new product = /api/v1/admin/product/new
 
 exports.newProduct = async (req, res, next) => {
@@ -23,10 +23,7 @@ exports.getProducts = async (req, res, next) => {
 exports.getSingleProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: 'Product not found',
-    });
+    return next(new ErrorHandler('Product not found', 404));
   }
   res.status(200).json({
     success: true,
@@ -39,10 +36,7 @@ exports.getSingleProduct = async (req, res, next) => {
 exports.updateProduct = async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: 'Product not found',
-    });
+    return next(new ErrorHandler('Product not found', 404));
   }
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -59,10 +53,7 @@ exports.updateProduct = async (req, res, next) => {
 exports.deleteProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: 'Product not found',
-    });
+    return next(new ErrorHandler('Product not found', 404));
   }
   await product.deleteOne();
   res.status(200).json({
