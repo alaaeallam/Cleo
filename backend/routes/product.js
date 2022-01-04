ecaae59df0d8d660e12a97f1cb38eb3a4a7cc3df
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-01-04 15:35:29 +0200

    Authorize user roleand permissions

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -8,12 +8,18 @@ const {
   updateProduct,
   deleteProduct,
 } = require('../controllers/productControllers');
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
-router.route('/products').get(getProducts);
+router.route('/products').get(isAuthenticatedUser, getProducts);
 router.route('/product/:id').get(getSingleProduct);
-router.route('/admin/product/new').post(isAuthenticatedUser, newProduct);
-router.route('/admin/product/:id').put(isAuthenticatedUser, updateProduct);
-router.route('/admin/product/:id').delete(isAuthenticatedUser, deleteProduct);
+router
+  .route('/admin/product/new')
+  .post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
+router
+  .route('/admin/product/:id')
+  .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
+router
+  .route('/admin/product/:id')
+  .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;
