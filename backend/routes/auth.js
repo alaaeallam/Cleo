a589d8658a1f6d18b384c5684eb8b38cd0abe53d
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-01-06 12:46:44 +0200

    Get user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,13 +6,17 @@ const {
   loginUser,
   forgetPassword,
   resetPassword,
+  getUserProfile,
   logout,
 } = require('../controllers/authController');
+
+const { isAuthenticatedUser } = require('../middleware/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/password/forget').post(forgetPassword);
 router.route('/password/reset/:token').put(resetPassword);
 router.route('/logout').get(logout);
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
 
 module.exports = router;
