4e2590412fab081ae9f54effaf43ae6a50a31a96
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-01-08 21:55:21 +0200

    Get all orders fromo admin route

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -31,3 +31,43 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+//Get single order => /api/v1/order/:id
+
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+  if (!order) {
+    return next(new ErrorHandler('Order not found ', 404));
+  }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+//Get logged in user orders => /api/v1/orders/me
+
+exports.myOrder = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id });
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+//Get all orders => /api/v1/admin/orders
+
+exports.allOrder = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder } = require('../controllers/orderControllers');
+const {
+  newOrder,
+  getSingleOrder,
+  myOrder,
+  allOrder,
+} = require('../controllers/orderControllers');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+router.route('/orders/me').get(isAuthenticatedUser, myOrder);
+router
+  .route('/admin/orders/')
+  .get(isAuthenticatedUser, authorizeRoles('admin'), allOrder);
 
 module.exports = router;
