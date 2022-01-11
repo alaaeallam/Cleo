6a1aa0bdb63c40a19b3aea1538c872446b39000f
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-01-11 14:31:37 +0200

    All product reducer2

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -17,8 +17,6 @@ export const productsReducer = (state = { products: [] }, action) => {
         loading: false,
         products: action.payload.products,
         productsCount: action.payload.productsCount,
-        resPerPage: action.payload.resPerPage,
-        filteredProductsCount: action.payload.filteredProductsCount,
       };
 
     case ALL_PRODUCTS_FAIL:
