d332eaa1456be693cb6eb94b2822695d0cfdf87d
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-01-12 22:16:33 +0200

    Product details reducer & action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,6 +4,9 @@ import {
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_FAIL,
   CLEAR_ERRORS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
 export const getProducts = () => async (dispatch) => {
@@ -22,6 +25,21 @@ export const getProducts = () => async (dispatch) => {
   }
 };
 
+export const getProductsDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/v1/products${id}`);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data.product,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      error: error.response.data.message,
+    });
+  }
+};
 // Clear Errors
 
 export const clearErrors = () => async (dispatch) => {
