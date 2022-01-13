fef9014ae23ac190efa18150b9b00bbea066a794
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-01-13 22:14:59 +0200

    Product Details Component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -28,7 +28,7 @@ export const getProducts = () => async (dispatch) => {
 export const getProductsDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
-    const { data } = await axios.get(`/api/v1/products${id}`);
+    const { data } = await axios.get(`/api/v1/product/${id}`);
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
       payload: data.product,
